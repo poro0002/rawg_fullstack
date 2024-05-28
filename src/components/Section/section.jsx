c530f1bb97f01396131584750a6d2bb1667ffc28
Kieran Poropat <kieran.poropat@gmail.com>
2024-05-28 12:24:21 -0400

    added respective logos for each section ga me

--- src/components/Section/section.jsx
+++ src/components/Section/section.jsx
@@ -115,7 +115,8 @@ const sectionDescriptions = [
         title: 'Multiplayer Online Battle Arena (MOBA)',
         description: 'Dota 2 has a large esports scene, with teams from around the world playing in various professional leagues and tournaments. Valve organizes the Dota Pro Circuit, which are a series of tournaments that award qualification points for earning direct invitations to The International, the games premier tournament held annually. Internationals feature a crowdfunded prize money system that has seen amounts in upwards of US$40 million, making Dota 2 the most lucrative esport. Media coverage of most tournaments is done by a selection of on-site staff who provide commentary and analysis for the ongoing matches similar to traditional sporting events. In addition to playing live to audiences in arenas and stadiums, broadcasts of them are also streamed over the internet and sometimes simulcast on television, with several million in viewership numbers.',
         subtext: 'Valve',
-        href: 'https://www.dota2.com/home'
+        href: 'https://www.dota2.com/home',
+        logo: 'src/Content/logos/hd-dota-2-logo_full.png'
       },
    
     
@@ -123,7 +124,8 @@ const sectionDescriptions = [
         title: 'Action Role-Playing Third-Person Shooter Multiplayer',
         description: 'The concept for Warframe originated in 2000, when Digital Extremes began work on a new game titled Dark Sector. At the time, the company had been successful in supporting other developers and publishers and wanted to develop its game in-house. Dark Sector suffered several delays and was eventually released in 2008, having used some of the initial framework but far different from the original plan. By 2012, in the wake of the success of free-to-play games, the developers took their earlier Dark Sector ideas and art assets and incorporated them into a new project, their self-published Warframe.',
         subtext: 'Digital Extremes',
-        href: 'https://www.warframe.com/landing'
+        href: 'https://www.warframe.com/landing',
+        logo: 'src/Content/logos/warframe_full.png'
         }
    ,
 
@@ -131,7 +133,8 @@ const sectionDescriptions = [
         title: 'Action-Adventure Video Game',
         description: 'Announced in March 2017, the game was released worldwide for PlayStation 4, Windows, and Xbox One on October 10, 2017. Shadow of War had a generally favorable reception from critics, albeit more mixed than its predecessor. Reviewers praised the gameplay and improved nemesis system, while criticizing the games microtransactions (which was removed in a post-game update in July 2018) and story, including changes to characters seen in Tolkiens books. Many critics noted that the games increased scope caused it to feel bloated. The game was the best-selling video game in the United States in its month of release. Monolith supported the game post-launch with free updates and two story expansions.',
         subtext: 'Monolith Productions',
-        href: 'https://www.shadowofwar.com/about/'
+        href: 'https://www.shadowofwar.com/about/',
+        logo: 'src/Content/logos/shadow_of_war_white_full.png'
       
      },
 
@@ -139,7 +142,8 @@ const sectionDescriptions = [
         title: 'Action Role-Playing Video Game',
         description: 'The games development began following the release of Assassins Creed IV: Black Flag in 2013. Ubisoft Montreal led its four-year development with help from a team of nearly 700 people from other Ubisoft studios around the world. The team consulted Egyptologists and historians extensively to ensure the setting was authentically represented in the game. In response to the common criticism that the gameplay of the series was getting stale and overly familiar, Ubisoft decided to reinvent the Assassins Creed formula with Origins. Whereas previous entries were mainly stealth-action games, Origins introduces many elements found in role-playing games and an overhauled hitbox-based combat system. While Assassins Creed had been an annual franchise since Assassins Creed II (2009), an extra year of development time allowed the team to polish the game further. This was largely a response to the tepid sales of Assassins Creed Syndicate, and the troubled launch of Assassins Creed Unity, which was plagued with technical issues when it was released in 2014.',
         subtext: 'Ubisoft Montreal',
-        href: 'https://www.ubisoft.com/en-ca/game/assassins-creed/shadows?ucid=SCH-ID_189303&maltcode=brand_C2W_SCH_googlesearch___RED____&addinfo=&gad_source=1&gclid=Cj0KCQjw3tCyBhDBARIsAEY0XNnR6iUfrWP6kxijQ8jQV965aisB-Ugsai8lu0mdWhiuxNfUrnml58oaAifyEALw_wcB'
+        href: 'https://www.ubisoft.com/en-ca/game/assassins-creed/shadows?ucid=SCH-ID_189303&maltcode=brand_C2W_SCH_googlesearch___RED____&addinfo=&gad_source=1&gclid=Cj0KCQjw3tCyBhDBARIsAEY0XNnR6iUfrWP6kxijQ8jQV965aisB-Ugsai8lu0mdWhiuxNfUrnml58oaAifyEALw_wcB',
+        logo: 'src/Content/logos/asc_full.png'
         },  
       
 ];
@@ -172,13 +176,22 @@ const sectionDescriptions = [
             <div className={`section-info__cont ${index % 2 === 0 ? 'even' : 'odd'}`}>
                 <div className="section-info-screenshot__cont">
                       
-                      <div className={`section-div__title ${index % 2 === 0 ? 'even' : 'odd'}`}>
-                            <h1 className='section__title'>{section.name}</h1>
-                            <p className="section__rating">Rating: <span className="section-info__value">{section.rating}</span></p>
-                            <p className="section__released">Released:  <span className="section-info__value">{section.released}</span></p>
-                            <p className="section__metacritic">Metacritic: <span className="section-info__value">{section.metacritic}</span> </p>
-                            <button onClick={() => seeGame(section)} className="section__btn" >View</button>
-                            <a className="section__href" href={sectionDescriptions[index].href}>Website</a>
+                    <div className={`section-div__title ${index % 2 === 0 ? 'even' : 'odd'}`}>
+                            
+                           
+
+                            <div className={`section-title-info__cont ${index % 2 === 0 ? 'even' : 'odd'}`}>
+                                <p className="section__rating">Rating: <span className="section-info__value">{section.rating}</span></p>
+                                <p className="section__released">Released:  <span className="section-info__value">{section.released}</span></p>
+                                <p className="section__metacritic">Metacritic: <span className="section-info__value">{section.metacritic}</span> </p>
+                                <button onClick={() => seeGame(section)} className="section__btn" >VIEW</button>
+                                <a className="section__href" href={sectionDescriptions[index].href}>WEBSITE</a>
+                            </div>
+
+                             <div className="section-title-logo__cont">
+                                <img className='section-title__logo' src={sectionDescriptions[index].logo} alt="" />
+                            </div>
+
                      </div>
 
                 
